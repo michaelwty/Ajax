d969507cc238cf40b953d53e15c9644c40b25695
michaelwty <wty1009@outlook.com>
2015-04-01 15:31:28 -0700

    end()

--- js/script.js
+++ js/script.js
@@ -14,13 +14,38 @@ function loadData() {
     // load streetview
     var $street = $('#street').val();
     var $city = $('#city').val();
-    var src = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + $street + ', ' + $city; // + '&fov=90&heading=235&pitch=10'
+    var imgSrc = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + $street + ', ' + $city;
+    $body.append('<img class="bgimg" src="' + imgSrc + '">');
+    $greeting.text('It\'s ' + $street + ' ' + $city + ' !');
 
-    //$body.prepend('<img id="theImg" src="" />');
-    //$('#theImg').attr('src', src);
-    $body.attr('background', src);
+    // NY Times Ajax Request
+    var apikey = '0024bfba89554124ae98c34700be7e14:19:71765882';
+    var requestNYT = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + $city + '&sort=newest&api-key=' + apikey;
+    $.getJSON(requestNYT, function (data) {
+        var dataset = data.response.docs;
+        $.each(dataset, function (index) {
+            $nytElem.append('<li id="nyt_artical_' + index + '"><a href="' + dataset[index].web_url + '">' + dataset[index].headline.main + '</a><br>' + dataset[index].snippet + '</li><br>');
+        });
+    }).error(function () {
+        $nytHeaderElem.text('Failed.');
+    });
 
-    // YOUR CODE GOES HERE!
+    //Wiki Ajax Request
+    var requestWiki = 'http://en.wikipedia.org/w/api.php?format=json&action=opensearch&search=' + $city;
+    var requestWikiTimeOut = setTimeout(function () {$wikiElem.text('Failed.');}, 3000);
+    $.ajax({
+        url: requestWiki,
+        dataType: 'jsonp',
+        success: function (wiki) {
+            console.log(wiki);
+            var wikidata = wiki[1];
+            for (var i = 1; i < wikidata.length; i++) {
+                $wikiElem.append('<li><a href="http://en.wikipedia.org/wiki/' + wikidata[i] + '">' + wikidata[i] + '</a></li><br>');
+            };
+            clearTimeout(requestWikiTimeOut);
+        }
+    });
+
 
     return false;
 };
